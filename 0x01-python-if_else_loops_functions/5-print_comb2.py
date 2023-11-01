3f57df81bfca8f9d86446836c4d072722d2980ec
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-10-31 23:57:30 -0700

    corrected

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range (100):
-    print(f"{i:02}", end=", " if i < 99 else "\n")
+     if i < 10:
+        print(f"0{i}", end=", ") 
+    else:
+        print(i, end=", ") 
