5d92f9029f67d8c3b18ccc912cce6aba5ad4120e
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-11-03 12:52:05 -0700

    late

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    import sys
-    count = len(sys.argv) -1 
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else: 
-        print("{}:{}".format(i + 1, sys.argv[i + 1]))
+import sys
+count = len(sys.argv) -1 
+if count == 0:
+print("0 arguments.")
+elif count == 1:
+print("1 argument:")
+else: 
+print("{}:{}".format(i + 1, sys.argv[i + 1]))
 
