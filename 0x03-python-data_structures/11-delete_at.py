c47c642d36bf63fb4a3c7b8e89f3b551471e36e4
jeremymordecai628 <jeremymordecai08@gmail.com>
2023-11-06 11:00:48 -0800

    hoo

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Delete an item at a specific position in a list"""
+    if my_list is None :
+        my_list []
+        if 0 <= idx <len(my_list) :
+            del my_list [idx]
+            return my_list
